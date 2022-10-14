ff8787e18e6628cabb62232f487fb53a2b1857d0
Jesse Kotsch <jessekotsch@sandiego.edu>
2022-10-14 11:05:03 -0700

    changes made

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -33,50 +33,45 @@ ServerSideSocket.bind((fakeIP, 0))
 
 
 while True:
-	try: 
-		print("Connecting Server")
-		ServerSideSocket.connect((serverIP,8080))
-		print("Listening for Client...")
-		connectionSocket, addr = ClientSideSocket.accept() ## RETURNS CONNECTION SOCKET
-	except Exception as e:
-		print(e)
-		time.sleep(5)
-		continue
-	else:
+	
+	print("Connecting Server")
+	ServerSideSocket.connect((serverIP,8080))
+	print("Listening for Client...")
+	connectionSocket, addr = ClientSideSocket.accept() ## RETURNS CONNECTION SOCKET
 
-		while True:
+	while True:
 
 
-			print("Client Connected: Ready to recieve message")
-			message = connectionSocket.recv(bufferSize)
-			# if the connection with the client sdrops before the first message is sent then close the connection and start over
-			if not message:
-				print("Client Connection Has Beed Lost. Closing Connections...")
-				break
-			else:
-				print("Message Received...")
-				print(message)
+		print("Client Connected: Ready to recieve message")
+		message = connectionSocket.recv(bufferSize)
+		# if the connection with the client sdrops before the first message is sent then close the connection and start over
+		if not message:
+			print("Client Connection Has Beed Lost. Closing Connections...")
+			break
+		else:
+			print("Message Received...")
+			print(message)
 	
 
 
-		#b. send received message from client
-				#b. send received message from client
-			print("Sending Message...")
-			try:
-				ServerSideSocket.send(message)
-			except:
-				print("Server Connection Has Beed Lost. Closing Connections...")
-				break
+	#b. send received message from client
+			#b. send received message from client
+		print("Sending Message...")
+		try:
+			ServerSideSocket.send(message)
+		except:
+			print("Server Connection Has Beed Lost. Closing Connections...")
+			break
 
 
     
-		print("Closing connection socket")
-		connectionSocket.close()
-		#close socket connection
+	print("Closing connection socket")
+	connectionSocket.close()
+	#close socket connection
 
-		# Assuming socket connection never fails for preliminary stage 
-		print("Closing Server Side Socket...")
-		ServerSideSocket.close()
+	# Assuming socket connection never fails for preliminary stage 
+	#print("Closing Server Side Socket...")
+	#ServerSideSocket.close()
 
 
 
