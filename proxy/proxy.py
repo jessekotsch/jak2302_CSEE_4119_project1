256875a9565384e5a6030dd24d228ac8751e5fca
Jesse Kotsch <jessekotsch@sandiego.edu>
2022-11-08 11:02:32 -0800

    Changes

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -9,8 +9,8 @@ import xml.etree.ElementTree as ET
 
 class Proxy:
 
-	def __init__(self):
-		pass
+	def __init__(self, identifier):
+		self.identifier = identifier
 
 
 	def bitrate_select(self, T_curr, availible_bitrates):
@@ -235,9 +235,9 @@ if __name__ == '__main__':
 			print("Receiving Response....")
 			connectionSocket.send(response)
 
-			header, body = Proxy.parse_header(str(response))
+			header, body = Proxy(0).parse_header(str(response))
 
-			content_length, partial_flag = Proxy.find_content_length(header)
+			content_length, partial_flag = Proxy(0).find_content_length(header)
 
 			print("Conetent length:" + str(content_length))
 			print("Partial Content:" + str(partial_flag))
@@ -247,7 +247,7 @@ if __name__ == '__main__':
 				while True:
 					temp_response = WebServerSideSocket.recv(bufferSize)
 					connectionSocket.send(temp_response)
-					temp_header, temp_body  = Proxy.parse_header(str(response))
+					temp_header, temp_body  = Proxy(0).parse_header(str(response))
 					total_received += len(temp_body)
 					body += temp_body
 					print("Body:")
@@ -264,7 +264,7 @@ if __name__ == '__main__':
 			# At beginning search minifest file for availible bitrates
 			if 'mpd' in url:
 				print("Parsing Manifest")
-				availible_bitrates = Proxy.bitrate_search(header)
+				availible_bitrates = Proxy(0).bitrate_search(header)
 
 				# Initialize current bitrate to lowest bitrate 
 				bitrate = min(availible_bitrates)
@@ -275,9 +275,9 @@ if __name__ == '__main__':
 
 			else:
 				print("Calculating Throughput")
-				T_new = Proxy.throughput_calc(beta, ftime, stime)
-				T_curr = Proxy.ewma_calc(T_curr, alpha, T_new)
-				bitrate = Proxy.bitrate_select(T_curr, availible_bitrates)
+				T_new = Proxy(0).throughput_calc(beta, ftime, stime)
+				T_curr = Proxy(0).ewma_calc(T_curr, alpha, T_new)
+				bitrate = Proxy(0).bitrate_select(T_curr, availible_bitrates)
 			
 			
 
@@ -286,7 +286,7 @@ if __name__ == '__main__':
 			print("Response Sent")
 
 			print("output log:")
-			Proxy.log_data(stime, ftime)
+			Proxy(0).log_data(stime, ftime)
 
 
 		except Exception as e:
