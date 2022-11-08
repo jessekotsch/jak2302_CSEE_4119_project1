096285877c29942f856193aac517a5822b3d10f1
Jesse Kotsch <jessekotsch@sandiego.edu>
2022-11-08 07:12:19 -0800

    Changes

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
 
 			response = WebServerSideSocket.recv(bufferSize)
 
-			Proxy(listenPort, fakeIP, webserverIP).parse_header(self, HTTP_mesaage)
+			Proxy(listenPort, fakeIP, webserverIP).parse_header(response)
 
 			#content_lingth = Proxy(listenPort, fakeIP, webserverIP).find_content_length(str(response))
 
