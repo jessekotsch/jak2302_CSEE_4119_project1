a4271d5539f2af01f71b2574e5f278b23e5d1797
Jesse Kotsch <jessekotsch@sandiego.edu>
2022-11-08 17:10:26 -0800

    Changes

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -339,11 +339,17 @@ if __name__ == '__main__':
 				print(e)
 				time.sleep(3)
 	
-
 	except Exception as e:
 		print("An Error Has Occured:")
 		print(e)
-		time.sleep(3)
+		# Close client and sever connections and restart
+		print("Closing connection socket")
+		connectionSocket.close()
+		#close socket connection
+
+		# Assuming socket connection never fails for preliminary stage 
+		print("Closing Server Side Socket...")
+		ServerSideSocket.close()
 
 
 
@@ -366,4 +372,81 @@ if __name__ == '__main__':
 
 
 
+
+
+
+
+	#########################
+	# ORIGINAL PROXY CODE
+	# SAVE JUST IN CASE
+	########################
+	"""
+	#Inputs: <listen-port> <fake-ip> <server-ip>
+	listenPort = int(sys.argv[1])
+	fakeIP = sys.argv[2]
+	serverIP = sys.argv[3]
+
+	bufferSize = 8
+
+
+
+	# Create client side socket, bind and listen 
+	print("Connecting Client")
+	ClientSideSocket = socket(AF_INET, SOCK_STREAM)
+	ClientSideSocket.bind(('', listenPort))
+	ClientSideSocket.listen(10)
+
+
+	while True:
+		try:
+			# Connect to server  
+			print("Connecting Server")
+			ServerSideSocket = socket(AF_INET, SOCK_STREAM)
+			ServerSideSocket.bind((fakeIP, 0))
+			ServerSideSocket.connect((serverIP,8080))
+		
+		except Exception as e:
+			# If server is not running print error and keep tryin to connect
+			print(e)
+			time.sleep(3)
+		else:
+			# if Server connects then wait for client 
+			print("Listening for Client...")
+			connectionSocket, addr = ClientSideSocket.accept() ## RETURNS CONNECTION SOCKET
+
+			while True:
+            
+				# client has connected to wait until message is recieved 
+				print("Client Connected: Ready to recieve message")
+				message = connectionSocket.recv(bufferSize)
+				# if the connection with the client drops before the first message is sent then close the connection and start over
+				if not message:
+					print("Client Connection Has Beed Lost. Closing Connections...")
+					break
+				else:
+					print("Message Received...")
+					print(message)
+	
+
+				# try sending the message to the server
+				try:
+					ServerSideSocket.send(message)
+				except:
+					# If message fails to send the server has been disconnected
+					print("Server Connection Has Beed Lost. Closing Connections...")
+					break
+
+
+			# Close client and sever connections and restart
+			print("Closing connection socket")
+			connectionSocket.close()
+			#close socket connection
+
+			# Assuming socket connection never fails for preliminary stage 
+			print("Closing Server Side Socket...")
+			ServerSideSocket.close()
+
+
+
+	"""
 
