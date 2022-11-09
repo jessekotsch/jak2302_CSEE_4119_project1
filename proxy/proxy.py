8b93320ec3a2e368a8b2bd9fb95a50f997b705d4
Jesse Kotsch <jessekotsch@sandiego.edu>
2022-11-09 09:37:47 -0800

    Changes

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -338,6 +338,7 @@ if __name__ == '__main__':
 							temp_response = WebServerSideSocket.recv(bufferSize)
 							total_received += len(temp_response)
 							response += temp_response
+							print("REsponse length: " ,len(response))
 							if len(temp_response) < bufferSize: break
 
 
